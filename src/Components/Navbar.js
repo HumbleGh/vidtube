4eb829bad69f9b3357366bacaaff6cd9490e4558
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2021-09-15 15:32:37 +0000

    'starting'

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css'
+
+
+const Navbar = () => {
+return (
+    <div className="bg-black-400">
+    <p> Hello world </p>
+</div>
+)
+}
+
+
+
+
+export default Navbar;
+
