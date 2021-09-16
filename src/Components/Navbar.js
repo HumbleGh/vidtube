f393614dc8216d8a02315105e79eeafdad662022
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2021-09-16 01:41:35 +0000

    on it

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,20 +2,32 @@ import React from 'react';
 import './Navbar.css';
 import menu from '../Assets/menu.png';
 import logo from '../Assets/logo.png';
+import upload from '../Assets/upload.png';
+import more from '../Assets/more.png';
+import notification from '../Assets/notification.png';
+import Jack from '../Assets/Jack.png';
+
 
 
 const Navbar = () => {
     return (
-        <div className='nav-left'>
-            <img className='' src={menu} alt='image'></img>
-            <img className='' src={logo} alt='image'></img>
-            <div className='nav-middle'>
-                <div className='nav-right'>
+        <div class="flex-div"> 
+             <div className='nav-left'>
+                <img className='' src={menu} alt='image'></img>
+                <img className='' src={logo} alt='image'></img>
+             </div>   
+                <div 
+                className='nav-middle'>
                 </div>
-            </div>
+                    <div className='nav-right'>
+                    <img className='' src={upload} alt='image'></img>
+                    <img className='' src={more} alt='image'></img>
+                    <img className='' src={notification} alt='image'></img>
+                    <img className='' src={Jack} alt='image'></img>                   
+                    </div>
         </div>
     )
-}
+};
 
 
 
